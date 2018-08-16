a4e4f99f2080f5d1ab56dcc34a5c038bd38cf81d
Afshin Teymoori <afshin_tm@yahoo.com>
2018-08-16 12:26:20 +1000

    add Autofac, Readme file and setup ApiEndpoint in Configuration

--- LetsBet.Api/LetsBet.Api/Startup.cs
+++ LetsBet.Api/LetsBet.Api/Startup.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using LetsBet.BusinessServices;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -11,30 +14,109 @@ using Microsoft.Extensions.Options;
 
 namespace LetsBet.Api
 {
+    //public class Startup
+    //{
+    //    public Startup(IConfiguration configuration)
+    //    {
+    //        Configuration = configuration;
+    //    }
+
+    //    public IConfiguration Configuration { get; }
+
+    //    // This method gets called by the runtime. Use this method to add services to the container.
+    //    public void ConfigureServices(IServiceCollection services)
+    //    {
+    //        services.AddMvc();
+    //    }
+
+    //    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    //    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    //    {
+    //        if (env.IsDevelopment())
+    //        {
+    //            app.UseDeveloperExceptionPage();
+    //        }
+
+    //        app.UseMvc();
+    //    }
+    //}
+    //=====================================
+
+    //==========================================
+
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            this.Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+        public IContainer ApplicationContainer { get; private set; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IConfigurationRoot Configuration { get; private set; }
+
+        // ConfigureServices is where you register dependencies. This gets
+        // called by the runtime before the Configure method, below.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
+            // Add services to the collection.
+            services.AddCors();
             services.AddMvc();
+
+            // Create the container builder.
+            var builder = new ContainerBuilder();
+
+            // Register dependencies, populate the services from
+            // the collection, and build the container. If you want
+            // to dispose of the container at the end of the app,
+            // be sure to keep a reference to it as a property or field.
+            //
+            // Note that Populate is basically a foreach to add things
+            // into Autofac that are in the collection. If you register
+            // things in Autofac BEFORE Populate then the stuff in the
+            // ServiceCollection can override those things; if you register
+            // AFTER Populate those registrations can override things
+            // in the ServiceCollection. Mix and match as needed.
+            builder.Populate(services);
+            builder.RegisterType<HttpClientManager>().As<IHttpClientManager>().SingleInstance();
+            builder.RegisterGeneric(typeof(BaseBusinessServices<>)).As(typeof(IBaseBusinessService<>)).InstancePerLifetimeScope();
+            builder.RegisterType<CustomerBusinessServices>().As<ICustomerBusinessServices>().InstancePerLifetimeScope();
+            builder.RegisterType<BetBusinessServices>().As<IBetBusinessServices>().InstancePerLifetimeScope();
+
+
+            this.ApplicationContainer = builder.Build();
+
+            // Create the IServiceProvider based on the container.
+            return new AutofacServiceProvider(this.ApplicationContainer);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        // Configure is where you add middleware. This is called after
+        // ConfigureServices. You can use IApplicationBuilder.ApplicationServices
+        // here if you need to resolve things from the container.
+        public void Configure(
+          IApplicationBuilder app,
+          ILoggerFactory loggerFactory,
+          IApplicationLifetime appLifetime)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            loggerFactory.AddConsole(this.Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
 
+
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials());
             app.UseMvc();
+            // If you want to dispose of resources that have been resolved in the
+            // application container, register for the "ApplicationStopped" event.
+            // You can only do this if you have a direct reference to the container,
+            // so it won't work with the above ConfigureContainer mechanism.
+            appLifetime.ApplicationStopped.Register(() => this.ApplicationContainer.Dispose());
         }
     }
+
+
+
 }
---
+++ README.md
@@ -0,0 +1,25 @@
+# README #
+
+
+### What is this repository for? ###
+
+This is a simple .NET Core 2 web api and an angularJs front-end applications for the technical challenge I have been asked to do.
+
+
+
+### Ge up and running with Web Api app ###
+
+Webapi has got 4 projects. Autofac is Dependency Injection used. 
+
+1- Model Layer, 
+2- Business Services Layer
+3- Web APi layer
+4- Unit testing -  for Business Services unit testing
+
+To get the web api application up and running just clone it and open Backend.sln using visual studio 2017 then Build and run it.
+
+##Api Endpoint:##
+
+
+### Ge up and running with front-end ###
+A simple angularJs 1.6 project to show the Race detail report
